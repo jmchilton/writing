dc6fe13e61da79d9106a9006337ba75f4acbd75d
John Chilton <jmchilton@gmail.com>
2013-08-14 11:07:06 -0500

    Update authors and copyright.

--- msi-galaxy-p/source/conf.py
+++ msi-galaxy-p/source/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Galaxy-P User Guide'
-copyright = '2013, John Chilton'
+copyright = '2013, Minnesota Supercomputing Institute'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -187,7 +187,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'UsingGalaxy-P.tex', 'Using Galaxy-P Documentation',
-   'John Chilton', 'manual'),
+   'John Chilton, Pratik Jagtap', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -217,7 +217,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'usinggalaxy-p', 'Using Galaxy-P Documentation',
-     ['John Chilton'], 1)
+     ['John Chilton, Pratik Jagtap'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -231,7 +231,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'UsingGalaxy-P', 'Using Galaxy-P Documentation',
-   'John Chilton', 'UsingGalaxy-P', 'One line description of project.',
+   'John Chilton, Pratik Jagtap', 'UsingGalaxy-P', 'One line description of project.',
    'Miscellaneous'),
 ]
 
